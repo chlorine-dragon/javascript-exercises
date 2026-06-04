3af0a305f0f16b28a49320c6c720edeea2d2b285
chlorine-dragon <289412665+chlorine-dragon@users.noreply.github.com>
2026-06-04 21:30:03 +0200

    tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(t) {
+  let cel = Math.round(((t - 32) * 5/9)*10)/10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(t) {
+  let far = Math.round((t * 9/5 + 32)*10)/10;
+  return far
 };
 
 // Do not edit below this line
