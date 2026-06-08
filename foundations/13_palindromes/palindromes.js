739df08405cf46ccf53d8823dab3663904d29fe9
chlorine-dragon <289412665+chlorine-dragon@users.noreply.github.com>
2026-06-08 14:59:57 +0200

    palindromes.js

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  // word = word.replacereplace(/[^\w\s\']|_/g, "")
+  //        .replace(/\s+/g, " ");
+  let arr = word.toLowerCase().replace(/[^a-z0-9]/g, "").split("");
+  for(let i=1; i <= Math.trunc(arr.length/2); i++){
+    if(arr[i] == arr.at(-i-1)){
+    }else{
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
