62a28cf1661dc2bf5562ac057fd2caad17b71f84
chlorine-dragon <289412665+chlorine-dragon@users.noreply.github.com>
2026-06-08 15:21:23 +0200

    fibonacci.js

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = Number(n);
+  let fib = [1,1];
+  let i = 0;
+  if(n < 0){
+    return 'OOPS'
+  }else if(n==0){
+    return 0;
+  }else{
+    while(i < n+1){
+      fib.push(fib[i] + fib[i+1]);
+      i++;
+    }
+  }
+  return(fib[n-1]);
 };
 
 // Do not edit below this line
