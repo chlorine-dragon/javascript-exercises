4d7bb37894faad96548a33162d41ef85ac38ed56
chlorine-dragon <289412665+chlorine-dragon@users.noreply.github.com>
2026-06-10 15:22:13 +0200

    findTheOldest.js

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+// const findTheOldest = function(arr) {
+//
+//   const currentYear = new Date().getFullYear();
+//   const ages = arr.map(({ yearOfBirth, yearOfDeath }) =>
+//   (yearOfDeath ?? currentYear - yearOfBirth
+// );
+//   const maxAge = Math.max(...ages);
+//   const oldest = arr[ages.indexOf(maxAge)];
+//   return oldest.name;
+// };
 
+const findTheOldest = function(arr) {
+  const currentYear = new Date().getFullYear();
+
+  const oldest = arr.reduce((oldest, person) => {
+    const agePerson = (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
+    const ageOldest = (oldest.yearOfDeath ?? currentYear) - oldest.yearOfBirth;
+    return agePerson > ageOldest ? person : oldest;
+  });
+
+  return oldest;
 };
 
 // Do not edit below this line
